fb0688c1ce66cdf21d6ea97cfacad960b8e30fbe
Sam McCathie <77808434+Sam-McCathie@users.noreply.github.com>
2025-03-31 15:35:41 +1300

    Setup DB connection and make basic query
    - I inserted some sample data via mysql workbench to verify.

--- server/index.php
+++ server/index.php
@@ -11,6 +11,20 @@
         exit;
     }
 
+    $dsn = 'mysql:host=mysql;port=3306;dbname=mydatabase;charset=utf8mb4';
+    $username = 'user';
+    $password = 'password';
+
+    // Create PDO instance database connection
+    try {
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(['error' => 'Database connection failed']);
+        exit;
+    }
+
     $requestUri = $_SERVER['REQUEST_URI'];
     $requestMethod = $_SERVER['REQUEST_METHOD'];
 
@@ -19,8 +33,12 @@
             $rawBody = file_get_contents("php://input");
             $jsonData = json_decode($rawBody, true);
 
-            echo json_encode($jsonData);
+            $stmt = $pdo->query("SELECT * FROM companies WHERE company_id = 1");
+            $company = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            echo json_encode($company);
             exit;
+            
         }
     } else {
         http_response_code(405);
